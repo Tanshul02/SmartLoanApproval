bbc138a97cb43c0a803b9ad8da038507195fb360
Tanshul02 <tanshulsharma02@gmail.com>
2026-09-10 18:28:04 +0530

    Add Customer class with attributes and getters

---
+++ src/main/java/com/loan/app/Customer.java
@@ -0,0 +1,53 @@
+package com.loan.app;
+
+public class Customer {
+
+    private String customerId;
+    private String name;
+    private int age;
+    private String governmentId;
+    private double monthlyIncome;
+    private int creditScore;
+    private double existingMonthlyObligations;
+
+    public Customer(String customerId, String name, int age,
+                    String governmentId, double monthlyIncome,
+                    int creditScore, double existingMonthlyObligations) {
+
+        this.customerId = customerId;
+        this.name = name;
+        this.age = age;
+        this.governmentId = governmentId;
+        this.monthlyIncome = monthlyIncome;
+        this.creditScore = creditScore;
+        this.existingMonthlyObligations = existingMonthlyObligations;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGovernmentId() {
+        return governmentId;
+    }
+
+    public double getMonthlyIncome() {
+        return monthlyIncome;
+    }
+
+    public int getCreditScore() {
+        return creditScore;
+    }
+
+    public double getExistingMonthlyObligations() {
+        return existingMonthlyObligations;
+    }
+}
